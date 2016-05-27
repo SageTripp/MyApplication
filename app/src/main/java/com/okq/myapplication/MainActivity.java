f444b6767a90da32d17854ed12f5967c0acc5dbb
张世通 <zhangst0720@163.com>
2016-05-27 18:32:04 +0800

    创建

---
+++ app/src/main/java/com/okq/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.okq.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+            }
+        }).start();
+    }
+}
